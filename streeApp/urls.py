1a41116fbfcf05baef0488c8e73b16e0f753166b
MonadWizard <monad.wizard.r@gmail.com>
2021-11-25 06:44:55 +0600

    update 0.2.1

--- streeApp/urls.py
+++ streeApp/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('create/', views.createPost, name='createPost'),
+    path('', views.createPost, name='createPost'),
     path('update/', views.updatePost, name='updatePost'),
     path('update/<int:edit_id>', views.edit, name='edit'),
 
